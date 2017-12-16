ad8225276bf95b42ce7853439516cb4bcfbe7a79
Robert Varga <robert.varga@globallogic.com>
2017-12-16 15:22:18 +0100

    daco

--- tempdir/home.html--49015c208e.php
+++ tempdir/home.html--49015c208e.php
@@ -24,94 +24,63 @@ class Template49015c208e extends Latte\Runtime\Template
     <!-- Css -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
-
+	
 </head>
 
 <body background="img/slide-1.jpg" >
-
-	<section>
-		<h2 align="center">Rozpoznávanie tvárí</h2>
-		<div class="container about">
-			<div class="row">
-				<div class="col-md-6">
-					<iframe width="580" height="305" src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe>
-				</div>
-				<div class="col-md-6">
-					<iframe width="580" height="305" src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe>
-				</div>
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">Rozpoznavanie tvari</a>
+				<p href="" class="navbar-text"> Stranka na rozpoznavanie tvari </p>
 			</div>
 		</div>
-	</section>
+	</div>
+	
+	<div class="container">
+		
+		<section>
+			<h2 align="center">Rozpoznávanie tvárí</h2>
+			<div class="container about">
+				<div class="row">
+					<div class="col-md-6">
+						<iframe width="580" height="305" src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe>
+					</div>
+					<div class="col-md-6">
+						<iframe width="580" height="305" src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		
+		<!-- Galery -->
+		<section>
 
-
-	<!-- Galery -->
-	<section>
-		<h2 align="center">Neznáme osoby</h2>
-		<div class="container">
-			<div class="row">
+					<div class="row">
 <?php
 		$iterations = 0;
 		foreach ($users as $user) {
 ?>
-				<div class="col-sm-6 col-md-2">
-					<div class="thumbnail">
-						<img src="<?php
-			echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($photosCDN)) /* line 43 */;
-			echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($user->photo)) /* line 43 */ ?>" alt="" class="img-thumbnail">
-						<div class="caption">
-							<h3><?php echo LR\Filters::escapeHtmlText($user->name) /* line 45 */ ?></h3>
-						</div>
-					</div>
-				</div>
-
-
+						<div class="col-sm-6 col-md-2">
+							<div class="thumbnail">
+								<img src="<?php
+			echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($photosCDN)) /* line 52 */;
+			echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($user->photo)) /* line 52 */ ?>" alt="" class="img-thumbnail">
+								<div class="caption">
+									<h3><?php echo LR\Filters::escapeHtmlText($user->name) /* line 54 */ ?></h3>
+								</div>
+							</div>
+						</div>								
 <?php
 			$iterations++;
 		}
 ?>
-				<!-- <div class="col-md-4 thumbnail">
-					<div class="hovereffect">
-						<div class="col-md-12 photo-4"></div>
-						<a href="#mybox" data-toggle="modal" data-slide-to="3">
-							<div class="overlay">
-									Meno:<br>
-									<input type="text" name="name" autocomplete="off"><br>
-							</div>
-						</a>
 					</div>
-				</div>
-
-				<div class="col-md-4 thumbnail">
-					<div class="hovereffect">
-						<div class="col-md-12 photo-4"></div>
-						<a href="#mybox" data-toggle="modal" data-slide-to="3">
-							<div class="overlay">
-									Meno:<br>
-									<input type="text" name="name" autocomplete="off"><br>
-							</div>
-						</a>
-					</div>
-				</div>
-
-				<div class="col-md-4 thumbnail">
-					<div class="hovereffect">
-						<div class="col-md-12 photo-4"></div>
-						<a href="#mybox" data-toggle="modal" data-slide-to="3">
-							<div class="overlay">
-									Meno:<br>
-									<input type="text" name="name" autocomplete="off"><br>
-							</div>
-						</a>
-					</div>
-				</div> -->
-
-
-				</div>
-			</div>
-		</div>	
 	</section>
-
-
+	
+</div>
+	
     <!-- script -->
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -119,7 +88,7 @@ class Template49015c208e extends Latte\Runtime\Template
 	<script src="js/script.js"></script>
 	
 	
-
+	
 </body>
 </html><?php
 		return get_defined_vars();
@@ -129,7 +98,7 @@ class Template49015c208e extends Latte\Runtime\Template
 	function prepare()
 	{
 		extract($this->params);
-		if (isset($this->params['user'])) trigger_error('Variable $user overwritten in foreach on line 40');
+		if (isset($this->params['user'])) trigger_error('Variable $user overwritten in foreach on line 49');
 		
 	}
 
